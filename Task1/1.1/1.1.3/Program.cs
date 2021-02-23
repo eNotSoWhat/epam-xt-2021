380f99fdeb6b6f770f61273d4a13bc94765c5c2b
eNotSoWhat <79451841+eNotSoWhat@users.noreply.github.com>
2021-02-24 00:34:19 +0400

    ready for checking

--- Task1/1.1/1.1.3/Program.cs
+++ Task1/1.1/1.1.3/Program.cs
@@ -9,7 +9,6 @@ namespace task1
             int n = int.Parse(Console.ReadLine());
             int l = (n * 2 - 1) / 2 - 1;
             int r = l;
-
             for (int i = 0; i < n; i++)
             {
                 for (int j = 0; j < 2 * n; j++)
