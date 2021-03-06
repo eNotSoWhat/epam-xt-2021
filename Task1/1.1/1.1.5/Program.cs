806150c8c0af847c953ac1861ec79cd3e2d5e85e
DESKTOP-63SMRK9\kir <latanov.kirill@yandex.ru>
2021-03-06 15:13:38 +0400

    ready for checking

---
+++ Task1/1.1/1.1.5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace task1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int ans = 0;
+            for (int i = 1; i < 1001; i++)
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    ans += i;
+                }
+            Console.WriteLine(ans);
+        }
+    }
+}
