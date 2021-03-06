eb5653c8f3f3a184007e2c0c1461a7c35f8138c8
DESKTOP-63SMRK9\kir <latanov.kirill@yandex.ru>
2021-03-06 15:08:21 +0400

    ready for checking

---
+++ Task1/1.1/1.1.4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace task1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int k = 0; k < n; k++)
+            {
+                int l = n - 1;
+                int r = l;
+                for (int i = 0; i < k + 1; i++)
+                {
+                    for (int j = 0; j < 2 * n; j++)
+                    {
+                        if (j >= l && j <= r)
+                            Console.Write('*');
+                        else
+                            Console.Write(' ');
+                    }
+                    Console.WriteLine();
+                    l--; r++;
+                }
+            }
+        }
+    }
+}
