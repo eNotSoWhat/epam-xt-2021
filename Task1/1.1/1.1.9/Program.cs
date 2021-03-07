aa41a488fd47e980959cbaeb7396117226cea67d
DESKTOP-63SMRK9\kir <latanov.kirill@yandex.ru>
2021-03-07 19:24:51 +0400

    ready for checking

---
+++ Task1/1.1/1.1.9/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[8];
+            Random ran = new Random();
+            int sum = 0;
+            Console.WriteLine("Array: ");
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] = ran.Next(-100, 100);
+                if (a[i] >= 0)
+                    sum += a[i];
+                Console.Write(a[i] + " ");
+            }
+            Console.WriteLine('\n');
+            Console.WriteLine($"Sum = {sum}");
+        }
+    }
+}
