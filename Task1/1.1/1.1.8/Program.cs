c4805d8f182afcc8b4eb5c156a3cdbd5b6315d43
DESKTOP-63SMRK9\kir <latanov.kirill@yandex.ru>
2021-03-07 20:25:42 +0400

    ready for checking

---
+++ Task1/1.1/1.1.8/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,,] a = new int[10, 10, 2];
+            Random ran = new Random();
+            int tmp = 0;
+            Console.WriteLine("Array: ");
+            for (int k = 0; k < 2; k++)
+            {
+
+                for (int i = 0; i < 10; i++)
+                {
+                    for (int j = 0; j < 10; j++)
+                    {
+                        a[i, j, k] = ran.Next(-10, 10);
+                        Console.Write(a[i, j, k] + " ");
+                    }
+                    Console.WriteLine(); 
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Result: ");
+            for (int k = 0; k < 2; k++)
+            {
+
+                for (int i = 0; i < 10; i++)
+                {
+                    for (int j = 0; j < 10; j++)
+                    {
+                        if (a[i, j, k] > 0)
+                            a[i, j, k] = 0;
+                        Console.Write(a[i, j, k] + " ");
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
