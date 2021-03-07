f36b55ac3ba8faaf5b6cfad5da6c8f432411b1e1
eNot <latanov.kirill@yandex.ru>
2021-03-07 22:03:55 +0400

    ready for checking

---
+++ Task1/1.2/1.2.1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = Console.ReadLine();
+            string s2 = Console.ReadLine();
+            for (int i = 0; i < s1.Length; i++)
+            {
+                if (s2.IndexOf(s1[i]) != -1)
+                    Console.Write(s1[i]);
+                Console.Write(s1[i]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
