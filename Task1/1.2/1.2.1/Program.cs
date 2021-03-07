1d773adaf53e19789799c937922919c850220149
DESKTOP-63SMRK9\kir <latanov.kirill@yandex.ru>
2021-03-07 23:04:10 +0400

    ready for checking

---
+++ Task1/1.2/1.2.1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //precision 0.01
+            string txt = Console.ReadLine().Trim();
+            double sum = 0; int k = 0;
+            string[] words = txt.Split(new char[] { '.', ':', '-', ',', '!', '?', ' '});
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i].Length != 0)
+                {
+                    sum += words[i].Length;
+                    k++;
+                }
+            }
+            Console.WriteLine(string.Format("{0:N2}", sum / k));
+        }
+    }
+}
