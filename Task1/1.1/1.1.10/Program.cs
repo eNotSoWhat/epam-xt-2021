c0fdd76c98750a1d3d62d8cdb2c37bd5da0178a0
DESKTOP-63SMRK9\kir <latanov.kirill@yandex.ru>
2021-03-07 20:43:52 +0400

    ready for checking

---
+++ Task1/1.1/1.1.10/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] a = new int[3, 3];
+            Random ran = new Random();
+            Console.WriteLine("Array: ");
+            int sum = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    a[i, j] = ran.Next(-10, 10);
+                    if ((i + j) % 2 == 0)
+                        sum += a[i, j];
+                    Console.Write(a[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine($"Result: {sum}");
+        }
+    }
+}
