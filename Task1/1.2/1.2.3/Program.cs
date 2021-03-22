14608aa83fe5551f4ca2a08ee7b7daa6a320d033
DESKTOP-63SMRK9\kir <latanov.kirill@yandex.ru>
2021-03-22 10:35:00 +0400

    init

---
+++ Task1/1.2/1.2.3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //precision 0.01
+            string txt = Console.ReadLine().Trim();
+            string tmp; int k = 0;
+            string[] words = txt.Split(new char[] { '.', ':', '-', ',', '!', '?', ' '});
+            for (int i = 0; i < words.Length; i++)
+            {
+                tmp = words[i];
+                if (tmp[0] >= 'a' && tmp[0] <= 'z')
+                {
+                    k++;
+                }
+            }
+            Console.WriteLine(k);
+        }
+    }
+}
