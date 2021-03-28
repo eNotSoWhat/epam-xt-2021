6b978aba42561b78ef05a6da3a0e343b3af8ac04
eNotSoWhat <79451841+eNotSoWhat@users.noreply.github.com>
2021-03-28 23:01:15 +0400

    ready for checking

---
+++ Task2/2.1/2.1.1/Program.cs
@@ -0,0 +1,89 @@
+using System;
+
+namespace newString
+{
+    class StringArray
+    {
+        private int len;
+        private char[] arr;
+        public StringArray saInit()
+        {
+            StringArray x = new StringArray();
+            x.len = 0;
+            x.arr = new char[0];
+            return x;
+        }
+        public StringArray saRead()
+        {
+            StringArray x = new StringArray();
+            string s = Console.ReadLine();
+            x.len = s.Length;
+            char[] arr = new char[x.len];
+            for (int i = 0; i < x.len; i++)
+            {
+                arr[i] = s[i];
+            }
+            x.arr = arr;
+            return x;
+        }
+        public void saPrint()
+        {
+            Console.WriteLine($"Size: {len}");
+            Console.Write("StringArray: ");
+            for (int i = 0; i < len; i++)
+                Console.Write(arr[i]);
+            Console.WriteLine();
+        }
+        public int saFind(char c)
+        {
+            int f = -1;
+            for (int i = 0; i < len; i++)
+            {
+                if (arr[i] == c)
+                    f = i;
+            }
+            return f;
+        }
+        public int saCompare(StringArray x)
+        {
+            if (len < x.len)
+                return -1;
+            else if (x.len == len)
+            {
+                for (int i = 0; i < len; i++)
+                    if (arr[i] < x.arr[i])
+                        return -1;
+                    else if (arr[i] > x.arr[i])
+                        return 1;
+                return 0;
+            }
+            return 1;
+        }
+        public StringArray saConcatinate(StringArray x)
+        {
+            StringArray ans = new StringArray();
+            ans.len = len + x.len;
+            ans.arr = new char[ans.len];
+            for (int i = 0; i < len; i++)
+                ans.arr[i] = arr[i];
+            for (int i = len; i < ans.len; i++)
+                ans.arr[i] = x.arr[i - len];
+            return ans;
+        }
+        public void saReverse()
+        {
+            char[] ans = new char[len];
+            for (int i = 0; i < len; i++)
+                ans[i] = arr[len - 1 - i];
+            arr = ans;
+        }
+
+        public static void Main()
+        {
+            StringArray x = new StringArray();
+            StringArray y = new StringArray();
+            StringArray z = new StringArray();
+            x = x.saRead();
+        }
+    }
+}
