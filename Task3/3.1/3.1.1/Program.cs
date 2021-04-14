873fe42f5c75c8a9983984eb126806cab232d10a
eNotSoWhat <79451841+eNotSoWhat@users.noreply.github.com>
2021-04-14 22:53:53 +0400

    ready for checking

--- Task3/3.1/3.1.1/Program.cs
+++ Task3/3.1/3.1.1/Program.cs
@@ -7,21 +7,21 @@ namespace weakestLink
     {
         static void Main()
         {
-            Console.WriteLine("Введите N:");
+            Console.WriteLine("Insert N:");
             int n = Convert.ToInt32(Console.ReadLine());
             List <int> players = new List <int> ();
             for (int i = 0; i < n; i++)
                 players.Add(i);
             int round = 1;
-            Console.WriteLine("Введите, какой по счету человек будет вычеркнут каждый раунд:");
+            Console.WriteLine("Insert player position to remove:");
             int pos = Convert.ToInt32(Console.ReadLine());
             while (players.Count > 1 && pos <= players.Count)
             {
                 players.RemoveAt(pos - 1);
-                Console.WriteLine($"Раунд {round}. Вычеркнут человек. Людей осталось: {players.Count}");
+                Console.WriteLine($"Round {round}. Player removed. People to remove: {players.Count}.");
                 round++;
             }
-            Console.WriteLine("Игра окончена. Невозможно вычеркнуть больше людей");
+            Console.WriteLine("Game over. Impossible to remove.");
         }
     }
 }
