91d3793d41b40e9cc548fefd30c902c28cc43ac6
eNotSoWhat <79451841+eNotSoWhat@users.noreply.github.com>
2021-05-05 01:40:40 +0400

    ready for checking

---
+++ Task3/3.3/3.3.3/Pizzeria.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PizzaTime
+{
+    public class Pizzeria
+    {
+        public Dictionary<int, Pizza> menu = new Dictionary<int, Pizza>()
+        {
+            {1, new Pizza ("Treasure Island", 654) },
+            {2, new Pizza ("Volga", 582) },
+            {3, new Pizza ("Captain's", 582) }
+        };
+        public void Menu()
+        {
+            foreach(var i in menu)
+            {
+                Console.WriteLine(i.Key + ") " + i.Value.name + " - " + i.Value.price);
+            }
+        }
+        public Action<int, Pizza> Order;
+        public void Queue(int id, Pizza pizza)
+        {
+            Task.Factory.StartNew(() =>
+            {
+                Order(id, pizza);
+            });
+        }
+    }
+}
